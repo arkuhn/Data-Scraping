34c18b7d1a9eaef938685cab3e73ea7870990c1e
ark9719 <ark9719@rit.edu>
2016-04-25 19:42:21 -0400

    Starting to learn pythons built in interface tools because why not

--- yellowPages.py
+++ yellowPages.py
@@ -1,11 +1,17 @@
+
 __author__ = 'Adam'
 
 import requests
+import tkinter as tk
+from tkinter import *
 from bs4 import BeautifulSoup
+
+root = tk.Tk()
+
 
 def dataget(food, zipcode):
     r = requests.get("http://www.yellowpages.com/search?search_terms="+food+"&geo_location_terms="+zipcode+"")
-    soup = BeautifulSoup(r.content)
+    soup = BeautifulSoup(r.content, "html.parser")
     g_data = soup.find_all("div", {"class":"info"})
     for item in g_data:
         try:
@@ -32,10 +38,39 @@ def dict(word):
             print(item.contents[0].find_all("div", {"class":"scnt"}))
         except:
             pass
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+        root.title("A Simple Data Scrape")
+        root.resizable(width=0, height=0)
+        root.geometry('{}x{}'.format(800, 600))
+
+    def createWidgets(self):
+        #listb = Listbox(root)
+        #listb.pack()
+
+        e = Entry(root, width=50)
+        e.pack()
+
+        interest = e.get()
+        self.QUIT = tk.Button(self, text="Quit", fg="red",
+                                            command=root.destroy)
+        self.QUIT.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
 def main():
-    food = input("What are you looking for: ")
-    zipcode = input("What zipcode?: ")
-    dataget(food, zipcode)
+    #food = input("What are you looking for: ")
+    #zipcode = input("What zipcode?: ")
+    #dataget(food, zipcode)
+
+
+    app = Application(master=root)
+    app.mainloop()
     #word = input("Define what: ")
     #dict(word)
 
